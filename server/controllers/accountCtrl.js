5cb4c3ef78b2c73340f97f9c215de7d706cadd79
Ethan Sanders <ethanedu5@gmail.com>
2018-12-04 11:04:19 -0600

    added addUser endpoint

--- server/controllers/accountCtrl.js
+++ server/controllers/accountCtrl.js
@@ -1,6 +1,26 @@
 function checkStatus(req,res){
     res.status(200).json(req.session.status)
 }
+function addUser(req,res){
+    const db = req.app.get("db");
+    const {username,email,name,dominant_hand,organization,city,state,lat,lng,avatar} = req.body;
+    db.topspin_user.insert({
+        username,
+        email,
+        name,
+        dominant_hand,
+        organization,
+        city,
+        state,
+        lat,
+        lng,
+        avatar
+    }).then(newUser=>{
+        req.session.user = newUser;
+        res.status(200).send(newUser)
+    }).catch(console.log)
+}
 module.exports = {
-    checkStatus
+    checkStatus,
+    addUser
 }
--- server/masterRoutes.js
+++ server/masterRoutes.js
@@ -1,7 +1,8 @@
-const {checkStatus} = require("./controllers/accountCtrl");
+const {checkStatus, addUser} = require("./controllers/accountCtrl");
 module.exports = app =>{
     app.get("/api/test",(req,res)=>{
         res.status(200).json(req.session)
     })
     app.get("/api/status",checkStatus)
+    app.post("/api/user",addUser)
 }
