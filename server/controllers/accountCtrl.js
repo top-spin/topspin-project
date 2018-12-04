9d7c7baf50fd70fa67cd2f4dffbb2da5b38fdd17
Ethan Sanders <ethanedu5@gmail.com>
2018-12-04 16:24:24 -0600

    added input fields to NewUser

--- server/controllers/accountCtrl.js
+++ server/controllers/accountCtrl.js
@@ -5,6 +5,7 @@ function addUser(req,res){
     const db = req.app.get("db");
     const {username,email,name,dominant_hand,organization,city,state,lat,lng,avatar} = req.body;
     db.topspin_user.insert({
+        user_id:req.session.user.id,
         username,
         email,
         name,
@@ -17,6 +18,7 @@ function addUser(req,res){
         avatar
     }).then(newUser=>{
         req.session.user = newUser;
+        req.session.status = "loggedIn"
         res.status(200).send(newUser)
     }).catch(console.log)
 }
