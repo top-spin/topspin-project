ccb1e76a911de01aca9ab5dc9d01a0cd46f66d0e
Ethan Sanders <ethanedu5@gmail.com>
2018-12-03 23:23:08 -0600

    added conditional rendering to App.vue

--- server/controllers/authCtrl.js
+++ server/controllers/authCtrl.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const Auth0Strategy = require("passport-auth0");
-const { DOMAIN, CLIENT_ID, CLIENT_SECRET, REACT_APP_CLIENT} = process.env;
+const { DOMAIN, CLIENT_ID, CLIENT_SECRET, VUE_APP_CLIENT} = process.env;
 
 module.exports = app =>{
     app.use(passport.initialize());
@@ -32,6 +32,30 @@ module.exports = app =>{
     );
     app.get("/success", (req, res) => {
       /** This is where we would connect logged in user to db */
-        console.log("Successful")
+        const db = req.app.get('db')
+        db.topspin_user.find({
+            user_id: req.user.id
+        }).then(user=>{
+            // console.log("user: ",user)
+            if (user.length ===0){
+            // db.gather_users.insert({
+            //     user_id:req.user.id
+            // }).then(newUser=>{
+            //     console.log("adding new user")
+                
+            req.session.user = req.user;
+            req.session.status = "needsInfo";
+            
+            res.redirect(VUE_APP_CLIENT);
+            // }).catch(console.log)
+        } else {
+            // console.log("already have user")
+            // console.log('req.session before: ', req.session);
+                req.session.user = user[0];
+                // console.log('req.session after: ', req.session);
+                req.session.status = "loggedIn";
+            res.redirect(VUE_APP_CLIENT);
+            }
+        }).catch(console.log)
     });
 }
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div v-if="status === 'loggedIn'" id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/dashboard">Dashboard</router-link>
@@ -8,11 +8,40 @@
       <router-link to="/chat">Chat</router-link>
       <router-link to="/tournaments">Tournaments</router-link>
       <router-link to="/account">Account</router-link>
-
     </div>
     <router-view/>
   </div>
+  <div v-else-if="status === 'moreInfo'" id="app">
+    <MoreInfo></MoreInfo>
+  </div>
+  <div v-else id="app">
+    <LandingPage></LandingPage>
+  </div>
 </template>
+<script>
+import LandingPage from "./views/LandingPage.vue";
+import MoreInfo from "./views/MoreInfo.vue";
+import axios from "axios";
+export default {
+  name:"App",
+  components:{
+    LandingPage,
+    MoreInfo
+  },
+  data(){
+    return{
+      status:""
+    }
+  },
+  mounted(){
+      axios.get("/api/status").then(res=>{
+        console.log(res.data)
+        this.status = res.data;
+      }).catch(err=>{console.log(err)})
+    }
+
+}
+</script>
 
 <style>
 #app {
