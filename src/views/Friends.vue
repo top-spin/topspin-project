d119b5505037f5735e0c29e5dcd4f4fc12687c14
Jason Laico <jason.laico@gmail.com>
2018-12-05 14:21:12 -0600

    friends started

--- src/views/Friends.vue
+++ src/views/Friends.vue
@@ -1,5 +1,39 @@
 <template>
-  <div >
-    <h1>This is the Friends Page</h1>
+  <div>
+                <v-radio-group light v-model="showfollowing" :mandatory="false">
+              <v-radio color="primary" label="Following" :value="true"></v-radio>
+              <v-radio color="primary" label="Followers" :value="false"></v-radio>
+            </v-radio-group>
+        <h1 v-if="showfollowing"  class="text-xs-left">Following</h1>
+    <Followinglist v-if="showfollowing"/>
+
+    
+        <h1 v-if="showfollowers" class="text-xs-left">Followers</h1>
+
+        <Followerslist v-if="showfollowers"  />
+
   </div>
 </template>
+
+
+<script>
+import Followinglist from "@/components/Followinglist";
+import Followerslist from "@/components/Followerslist";
+export default {
+  name: "Friends",
+  components: {
+    Followinglist,
+    Followerslist
+  },
+  data() {
+    return {
+      showfollowing:true,
+      showfollowers:true
+    };
+  }
+};
+</script>
+
+<style>
+</style>
+
