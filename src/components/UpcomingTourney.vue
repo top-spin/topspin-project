993b35df9aaa8709f7e7e4c19236066621d44562
Ethan Sanders <ethanedu5@gmail.com>
2018-12-05 20:55:53 -0600

    added routing to tournament view from tournaments

--- src/components/UpcomingTourney.vue
+++ src/components/UpcomingTourney.vue
@@ -4,7 +4,42 @@
       <span class="font-weight-light">Upcoming</span>
       <span class="orange--text">Tournaments</span>
     </span>
-      <h1 v-for="tournament in tournaments" :key="tournament.id">{{tournament.name}}</h1>
+      <!-- <h1 v-for="tournament in tournaments" :key="tournament.id">{{tournament.name}}</h1> -->
+      <div subheader v-if="tournaments.length!==0">
+        <v-list>
+          <!-- <v-subheader>Recent chat</v-subheader> -->
+          <v-list-tile
+             v-for="(tournament,i) in tournaments" 
+            :key="i"
+            @click="selectTournament(tournament.tournament_id)"
+          >
+
+            <v-list-tile-content>
+              <v-list-tile-title v-html="tournament.tournament_name"></v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-list-tile-sub-title>Created By: {{tournament.creator}}</v-list-tile-sub-title>
+            </v-list-tile-action>
+
+          </v-list-tile>
+      </v-list>
+
+      <v-divider></v-divider>
+      </div>
+      <v-list subheader v-else>
+          <!-- <v-subheader>Recent chat</v-subheader> -->
+        <v-list-tile
+        >
+
+            <v-list-tile-content>
+              <v-list-tile-title class="xs-text-center">No Upcoming Tournaments</v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <!-- <v-list-tile-sub-title>Created By: {{tournament.creator}}</v-list-tile-sub-title> -->
+            </v-list-tile-action>
+
+          </v-list-tile>
+      </v-list>
   </div>
 </template>
 <script>
@@ -18,8 +53,14 @@ export default {
     },
     mounted(){
         axios.get("/api/upcoming-tournaments").then(res=>{
+            console.log(res.data)
             this.tournaments = res.data;
         }).catch(err=>console.log(err))
+    },
+    methods:{
+      selectTournament(id){
+        this.$router.push("/tournament/view/"+id)
+      }
     }
 }
 </script>
