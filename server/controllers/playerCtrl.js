b4e7a6a5e3e12ed357bf05381167d7f8c230d972
Jerry <kickassmusic4vr@hotmail.com>
2018-12-10 17:47:32 -0600

    Edit button for profile page

--- server/controllers/playerCtrl.js
+++ server/controllers/playerCtrl.js
@@ -1,41 +1,66 @@
-function profileInfo(req,res){
-    const db = req.app.get('db')
-    db.query(`
+function profileInfo(req, res) {
+  const db = req.app.get("db");
+  db.query(
+    `
     select * from (
         select row_number() over(order by rating desc) as rank,  * from topspin_user 
         order by rating desc
         ) as ur
         where ur.username = '${req.params.username}';
-    `).then(user=>{
-        // res.status(200).json(user)
-        
-        db.query(`
+    `
+  )
+    .then(user => {
+      // res.status(200).json(user)
+
+      db.query(
+        `
         select count(m.match_id) from topspin_user u
             join match m
             on m.match_winner = u.user_id
             where u.user_id = '${user[0].user_id}';
-        `).then(winCount=>{
-            db.query(`
+        `
+      )
+        .then(winCount => {
+          db.query(
+            `
             select count(m.match_id) from topspin_user u
             join match m
             on m.match_loser = u.user_id
             where u.user_id = '${user[0].user_id}';
-            `).then(lossCount=>{
-                // res.status(200).json(winCount[0].count+lossCount[0].count)
-                res.status(200).json({
-                    user:user[0],
-                    winCount:winCount[0].count,
-                    winPercent:winCount[0].count*100/(+winCount[0].count+(+lossCount[0].count)),
-                    rank:user[0].rank
-                })
-                
-            }).catch(console.log())
-        }).catch(console.log())
-    }).catch(console.log())
+            `
+          )
+            .then(lossCount => {
+              // res.status(200).json(winCount[0].count+lossCount[0].count)
+              res.status(200).json({
+                user: user[0],
+                winCount: winCount[0].count,
+                winPercent:
+                  (winCount[0].count * 100) /
+                  (+winCount[0].count + +lossCount[0].count),
+                rank: user[0].rank
+              });
+            })
+            .catch(console.log());
+        })
+        .catch(console.log());
+    })
+    .catch(console.log());
 }
-function searchPlayers(req,res){
-    const db = req.app.get('db')
-    db.query(`
+function updateProfile(req, res) {
+  const db = req.app.get("db");
+  db.topspin_user
+    .save(req.body)
+    .then(result => {
+      console.log("upate profile HIT");
+      res.status(200).json(result);
+    })
+    .catch(err => console.log(err));
+}
+
+function searchPlayers(req, res) {
+  const db = req.app.get("db");
+  db.query(
+    `
     select * from (
         select row_number() over(order by rating desc) as rank,  * from topspin_user 
         order by rating desc
@@ -43,21 +68,30 @@ function searchPlayers(req,res){
         where upper(ur.username) like '%${req.query.value.toUpperCase()}%' or
         upper(ur.name) like '%${req.query.value.toUpperCase()}%' or upper(email) like '%${req.query.value.toUpperCase()}%' or
         upper(ur.organization) like '%${req.query.value.toUpperCase()}%';
-    `).then(results=>{
-        res.status(200).json(results)
-    }).catch(err=>(console.log(err)))
+    `
+  )
+    .then(results => {
+      res.status(200).json(results);
+    })
+    .catch(err => console.log(err));
 }
-function getAllPlayers(req,res){
-    const db = req.app.get('db')
-    db.query(`
+function getAllPlayers(req, res) {
+  const db = req.app.get("db");
+  db.query(
+    `
     select row_number() over(order by rating desc) as rank,  * from 
         topspin_user order by rating desc;
-    `).then(results=>{
-        res.status(200).json(results)
-    }).catch(err=>(console.log(err)))
+    `
+  )
+    .then(results => {
+      res.status(200).json(results);
+    })
+    .catch(err => console.log(err));
 }
+
 module.exports = {
-    profileInfo,
-    searchPlayers,
-    getAllPlayers
-}
+  profileInfo,
+  updateProfile,
+  searchPlayers,
+  getAllPlayers
+};
--- src/store.js
+++ src/store.js
@@ -1,42 +1,53 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    status:"",
-    avatar:"https://www.w3schools.com/howto/img_avatar.png",
-    pending_tournament:{},
-    user:{}
+    status: "",
+    avatar: "https://www.w3schools.com/howto/img_avatar.png",
+    pending_tournament: {},
+    user: {}
   },
   mutations: {
-    SET_STATUS(state,status){
+    SET_STATUS(state, status) {
       state.status = status;
     },
-    SET_AVATAR(state,avatar){
+    SET_AVATAR(state, avatar) {
       state.avatar = avatar;
     },
-    SET_TOURNAMENT(state,pending_tournament){
+    SET_TOURNAMENT(state, pending_tournament) {
       state.pending_tournament = pending_tournament;
     },
-    SET_USER(state,user){
+    SET_USER(state, user) {
+      console.log(user);
       state.user = user;
     }
   },
   actions: {
-    getStatus(){
-      axios.get("/api/status").then(res=>{
-        //console.log(res.data)
-        this.commit('SET_STATUS',res.data)
-      }).catch(err=>{console.log(err)})
+    getStatus() {
+      axios
+        .get("/api/status")
+        .then(res => {
+          //console.log(res.data)
+          this.commit("SET_STATUS", res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     },
-    getUser(){
-      axios.get("/api/current-user").then(res=>{
-        //console.log(res.data)
-        this.commit('SET_USER',res.data)
-      }).catch(err=>{console.log(err)})
+    getUser() {
+      axios
+        .get("/api/current-user")
+        .then(res => {
+          //console.log(res.data)
+          this.commit("SET_USER", res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
-})
+});
